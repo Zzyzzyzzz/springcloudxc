0106ebf69d2a347c86f0d90c21e891976a95218b
Zzyzzyzzz <1048285986@qq.com>
2023-08-30 21:59:45 +0800

    gateway

---
+++ gateway-9999/src/main/java/com/xc/gateway9999/filter/MyFilter.java
@@ -0,0 +1,30 @@
+package com.xc.gateway9999.filter;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class MyFilter implements Ordered, GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if(StringUtils.isEmpty(username)){
+            System.out.println("用户名不能为空");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
